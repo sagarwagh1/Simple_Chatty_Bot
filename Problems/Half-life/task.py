eb23bec53be481b03bd707edd9962e4615d36fe1
sagarwagh1 <wsagar.wagh1@gmail.com>
2020-05-21 17:35:54 +0530

    Stage Description Added

--- Problems/Half-life/task.py
+++ Problems/Half-life/task.py
@@ -1,3 +1,34 @@
+"""
+In nuclear physics, the half-life is used to describe the rate with which elements undergo radioactive decay.
+More precisely, it is the time required for an element to reduce in half.
+Let's take an isotope of Radium (Ra) called radium-223. Say, its half-life is about 12 days.
+This means that every 12 days the number of atoms reduces in half.
+Write a program that calculates how many full half-life periods (in days) it would take for a certain amount of
+radium-223 to reduce to a specific value.
+
+The input format:
+The first line with the starting amount of atoms N (from 2 to 1,000,000), the second line with the resulting quantity R.
+The output format:
+The number of full half-life periods (in days) T it would take for radium-223 to reduce from N to R.
+
+Assume that any change would take at least 1 half-life period.
+Suppose, the initial number of atoms is 4 and the resulting quantity equals to 3. In 12 days,
+the number will reduce to 2 atoms. Since we are counting full half-life periods, you should write 12 and so on.
+
+Sample Input:
+4
+3
+Sample Output:
+12
+
+Sample Input:
+835950
+139505
+Sample Output:
+36
+"""
+# put your python code here
+
 N = int(input())
 R = int(input())
 T = 0
--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,3 +1,13 @@
-# write your code here
+"""
+Stage 1 Task:
+For the first stage, you will write a bot who displays a greeting, its name and the date of its creation.
+First impressions count!
+Your program should print the following lines:
+Hello! My name is {bot_name}.
+I was created in {birth_year}.
+Instead of {bot_name} print any name you choose and replace {birth_year} with the current year (four digits).
+"""
+# put your python code here
+
 print("Hello! My name is Aid")
 print("I was created in 2020")
