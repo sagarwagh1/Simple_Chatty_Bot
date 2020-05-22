8731ab0098c5decc15cd4834d9df43d891b5930d
sagarwagh1 <wsagar.wagh1@gmail.com>
2020-05-22 19:30:00 +0530

    Stage 2 Completed.

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,13 +1,18 @@
 """
-Stage 1 Task:
-For the first stage, you will write a bot who displays a greeting, its name and the date of its creation.
-First impressions count!
+At this stage, you will introduce yourself to the bot so that it can greet you by your name.
 Your program should print the following lines:
-Hello! My name is {bot_name}.
-I was created in {birth_year}.
-Instead of {bot_name} print any name you choose and replace {birth_year} with the current year (four digits).
+Hello! My name is Aid.
+I was created in 2020.
+Please, remind me your name.
+What a great name you have, {your_name}!
+You may change the name and the creation year of your bot if you want
 """
 # put your python code here
 
-print("Hello! My name is Aid")
-print("I was created in 2020")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+your_name = input()
+print('What a great name you have, ' + your_name + '!')
