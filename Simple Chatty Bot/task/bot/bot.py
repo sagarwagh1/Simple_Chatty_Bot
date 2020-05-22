d26b0e3d546e3dc84054370d89d247d2b6cc7a95
sagarwagh1 <wsagar.wagh1@gmail.com>
2020-05-22 19:40:34 +0530

    Stage 3 Completed.

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,18 +1,17 @@
-"""
-At this stage, you will introduce yourself to the bot so that it can greet you by your name.
-Your program should print the following lines:
-Hello! My name is Aid.
-I was created in 2020.
-Please, remind me your name.
-What a great name you have, {your_name}!
-You may change the name and the creation year of your bot if you want
-"""
-# put your python code here
-
 print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-# reading a name
-your_name = input()
-print('What a great name you have, ' + your_name + '!')
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
